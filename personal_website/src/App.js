c3d6b4d90acf879668871622e594ae0e677bb696
Anthony Micco <anthonymicco2003@gmail.com>
2023-05-25 13:05:11 -0400

    changed name of website

--- personal_website/src/App.js
+++ personal_website/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 
 function App() {
-  return (<div>
-    <h1>This is going to be an NFL player search website</h1>
-  </div>);
+  return (
+    <div>
+      <h1>This is going to be my personal website! </h1>
+    </div>);
 }
 
 export default App;
