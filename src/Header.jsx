7d681506762146059ab6d8a2a079f3058fc1eeac
Anthony Micco <anthonymicco2003@gmail.com>
2023-07-24 14:19:02 -0400

    updated header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,22 +1,36 @@
 import { faGithub, faLinkedin, faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+
 export function Header() {
     return (
         <section>
             <h1 className="name">Anthony Micco</h1>
             <p className="about">I'm an aspiring software engineer pursuing a computer<br></br>science degree at Youngstown State University </p>
             <img src="../portrait.jpg" alt="self portrait" />
-            <a className='github' href='https://github.com/Ajmicco18'>
-                <FontAwesomeIcon icon={faGithub} />
-            </a>
-            <a className='linkedin' href='https://linkedin.com/in/anthony-micco'>
-                <FontAwesomeIcon icon={faLinkedin} />
-            </a>
-            <a className='insta' href='https://www.instagram.com/anthony.micco/'>
-                <FontAwesomeIcon icon={faInstagram} />
-            </a>
+            <ul className="socials">
+                <li className="socialLink">
+                    <a className="github" href='https://github.com/Ajmicco18' target="_blank">
+                        <FontAwesomeIcon icon={faGithub} />
+                    </a>
+                </li>
+                <li className="socialLink">
+                    <a className="linkedin" href='https://linkedin.com/in/anthony-micco' target="_blank">
+                        <FontAwesomeIcon icon={faLinkedin} />
+                    </a>
+                </li>
+                <li className="socialLink">
+                    <a className="insta" href='https://www.instagram.com/anthony.micco/' target="_blank">
+                        <FontAwesomeIcon icon={faInstagram} />
+                    </a>
+                </li>
+                <li className="socialLink">
+                    <a href='../anthony_micco_resume.pdf' target="_blank">
+                        <button className="resume">Resume</button>
+                    </a>
 
+                </li>
+            </ul>
         </section>
 
     )
