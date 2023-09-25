4dc8f5ed4bc04ea310abeab0c2d36a8171d8c651
Anthony Micco <anthonymicco2003@gmail.com>
2023-09-25 11:23:50 -0400

    updated experience

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,7 +1,10 @@
 export function Experience() {
     return (
         <section className="experience">
-            <h1>hello</h1>
+            <div className="container">
+                <h1>Experience</h1>
+            </div>
+
         </section>
 
     )
