f76d3a7bac799450ffa04e0569900980f2035473
Anthony Micco <anthonymicco2003@gmail.com>
2023-09-28 13:25:52 -0400

    began work on experience

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -4,7 +4,18 @@ export function Experience() {
         <section className="experience">
             <div className="container">
                 <h1>Experience</h1>
+                <ul className="exp">
+                    <li id="bdmn">Boardman Local Schools</li>
+                    <li>Boardman Local Schools</li>
+                    <li>Clear Cut Stump Grinding</li>
+                </ul>
+                <p>Computer Technology Intern<br></br><br></br>
+                    As a computer technology intern for the Boardman Local Schools, I was responsible
+                    for responding to teacher's and staff's help-desk tickets involving their
+                    technology.
+                </p>
             </div>
+
 
         </section>
 
