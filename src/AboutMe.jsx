dd5f0c413f42075523c01d9a8986f6c35d7ac041
Anthony Micco <anthonymicco2003@gmail.com>
2023-09-28 11:24:04 -0400

    added some responsiveness

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -2,48 +2,39 @@ export function AboutMe() {
     return (
         <section className="about-me">
             <div className="container">
-                <div className="box-1">
-                    <h1>About Me</h1>
-                    <p>Hello! I am Anthony Micco. I was inspired to pursue a career in the
-                        computer science field after taking a computer science course during
-                        my sophomore year of high school. Since that time, I have devloped a passion
-                        for coding and devloping software and look forward to continuing to learn
-                        and grow as a devloper.
-                    </p>
-                    <p>
-                        I am currently going into my third year of school at Youngstown State University.
-                        I am working towards earning Bachelor of Science degree in computer science
-                        with an expected graduation of May 2025. I'm aspiring to forge a career
-                        in the broad field of computer science, I am eager to leverage my proficiency in
-                        programming languages and algorithm design to contribute to modern technological
-                        advancements. With a commitment to continuous learning and problem-solving prowess,
-                        I seek to excel as a versatile software engineer and make a meaningful impact in the industry.
-                    </p>
-                    <p>
-                        I currently work as a middle school football coach in the Boardman Local School District in
-                        Boardman, Ohio.
-                    </p>
+                <h1 className="about_title">About Me</h1>
+                <p className="about_para">Hello! I am Anthony Micco. I was inspired to pursue a career in the
+                    computer science field after taking a computer science course during
+                    my sophomore year of high school. Since that time, I have devloped a passion
+                    for coding and devloping software and look forward to continuing to learn
+                    and grow as a devloper.<br></br><br></br>
+                    I am currently going into my third year of school at Youngstown State University.
+                    I am working towards earning Bachelor of Science degree in computer science
+                    with an expected graduation of May 2025. I'm aspiring to forge a career
+                    in the broad field of computer science, I am eager to leverage my proficiency in
+                    programming languages and algorithm design to contribute to modern technological
+                    advancements. With a commitment to continuous learning and problem-solving prowess,
+                    I seek to excel as a versatile software engineer and make a meaningful impact in the industry.<br></br><br></br>
+                    I currently work as a middle school football coach in the Boardman Local School District in
+                    Boardman, Ohio.
+                </p>
 
-                </div>
+                <h1 className="skills">Skills</h1>
+                <ul className="tech">
+                    <li className="lang">Java</li>
+                    <li className="lang">C++</li>
+                    <li className="lang">Python</li>
+                    <li className="lang">SQL</li>
+                    <li className="lang">CSS</li>
+                    <li className="lang">HTML</li>
+                    <li className="lang">Javascript</li>
+                    <li className="lang">Typescript</li>
+                    <li className="lang">React</li>
+                    <li className="lang">R</li>
+                    <li className="lang">Git</li>
+                    <li className="lang">VS Code</li>
 
-                <div className="box-2">
-                    <h1 className="skills">Skills</h1>
-                    <ul className="tech">
-                        <li className="lang">Java</li>
-                        <li className="lang">C++</li>
-                        <li className="lang">Python</li>
-                        <li className="lang">SQL</li>
-                        <li className="lang">CSS</li>
-                        <li className="lang">HTML</li>
-                        <li className="lang">Javascript</li>
-                        <li className="lang">Typescript</li>
-                        <li className="lang">React</li>
-                        <li className="lang">R</li>
-                        <li className="lang">Git</li>
-                        <li className="lang">VS Code</li>
-
-                    </ul>
-                </div>
+                </ul>
             </div>
 
 
