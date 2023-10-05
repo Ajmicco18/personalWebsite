72171c2884dbc1c5f4525f75c86853065fcaa34f
Anthony Micco <anthonymicco2003@gmail.com>
2023-10-05 19:06:49 -0400

    testing

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,4 +1,10 @@
 export function Experience() {
+    function changeJob() {
+        var x = "This is a test";
+        document.getElementById("test").display.fontSize = "30px";
+        console.log(x);
+
+    }
 
     return (
 
@@ -10,7 +16,7 @@ export function Experience() {
                     <li>Boardman Local Schools</li>
                     <li>Clear Cut Stump Grinding</li>
                 </ul>
-                <p>Computer Technology Intern<br></br><br></br>
+                <p id="test" onClick={changeJob()}>Computer Technology Intern<br></br><br></br>
                     As a computer technology intern for the Boardman Local Schools, I was responsible
                     for responding to teacher's and staff's help-desk tickets involving their
                     technology.
