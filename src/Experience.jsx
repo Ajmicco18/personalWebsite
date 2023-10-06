02da44ff360cd631c00d702ab89f1924bf24fb8c
Anthony Micco <anthonymicco2003@gmail.com>
2023-10-05 20:01:47 -0400

    edited experience

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,9 +1,7 @@
 export function Experience() {
-    function changeJob() {
-        var x = "This is a test";
-        document.getElementById("test").display.fontSize = "30px";
+    function test() {
+        const x = "this is a test";
         console.log(x);
-
     }
 
     return (
@@ -12,11 +10,11 @@ export function Experience() {
             <div className="container">
                 <h1>Experience</h1>
                 <ul className="exp">
-                    <li>Boardman Local Schools</li>
-                    <li>Boardman Local Schools</li>
-                    <li>Clear Cut Stump Grinding</li>
+                    <button onClick={() => test()} >Boardman Local Schools</button>
+                    <button>Boardman Local Schools</button>
+                    <button>Clear Cut Stump Grinding</button>
                 </ul>
-                <p id="test" onClick={changeJob()}>Computer Technology Intern<br></br><br></br>
+                <p className="exp_para">Computer Technology Intern<br></br><br></br>
                     As a computer technology intern for the Boardman Local Schools, I was responsible
                     for responding to teacher's and staff's help-desk tickets involving their
                     technology.
