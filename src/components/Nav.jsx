872a83628bb3236a68942860384c7451113f4896
Anthony Micco <anthonymicco2003@gmail.com>
2023-12-19 11:25:15 -0500

    updated nav to be present while scrolling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,28 @@
-import { AppBar, Box, Toolbar, Typography, Button } from '@mui/material/';
+import { AppBar, Box, Toolbar, Typography, Button, useScrollTrigger } from '@mui/material/';
+import React from 'react';
+import PropTypes from "prop-types"
+
+function ElevationScroll(props) {
+    const { children, window } = props;
+    const trigger = useScrollTrigger({
+        disableHysteresis: true,
+        threshold: 0,
+        target: window ? window() : undefined,
+    });
+
+    return React.cloneElement(children, {
+        elevation: trigger ? 4 : 0,
+    });
+}
+
+ElevationScroll.propTypes = {
+    children: PropTypes.element.isRequired,
+    window: PropTypes.func,
+};
+
+export default function Nav(props) {
 
 
-export default function Nav() {
 
     const handleResume = () => {
         window.open("../images/anthony-micco-resume.pdf")
@@ -10,20 +31,22 @@ export default function Nav() {
     return (
         <>
             <Box sx={{ flexGrow: 1 }}>
-                <AppBar position="static" sx={{ backgroundColor: "rgb(63, 63, 63)" }}>
-                    <Toolbar>
-                        <Typography href="/Home"
-                            variant="h4"
-                            component="a"
-                            sx={{ flexGrow: 1, color: "skyblue", textDecoration: "none" }}>
-                            &lt;Anthony Micco/&gt;
-                        </Typography>
-                        <a href='/Projects'>
-                            <Button sx={{ fontSize: "xl", color: "skyblue" }}>Projects</Button>
-                        </a>
-                        <Button onClick={handleResume} sx={{ fontSize: "xl", color: "skyblue" }}>Resume</Button>
-                    </Toolbar>
-                </AppBar>
+                <ElevationScroll {...props}>
+                    <AppBar sx={{ backgroundColor: "rgb(63, 63, 63)" }}>
+                        <Toolbar>
+                            <Typography href="/Home"
+                                variant="h3"
+                                component="a"
+                                sx={{ flexGrow: 1, color: "skyblue", textDecoration: "none" }}>
+                                &lt;Anthony Micco/&gt;
+                            </Typography>
+                            <a href='/Projects'>
+                                <Button sx={{ fontSize: "xl", color: "skyblue" }}>Projects</Button>
+                            </a>
+                            <Button onClick={handleResume} sx={{ fontSize: "xl", color: "skyblue" }}>Resume</Button>
+                        </Toolbar>
+                    </AppBar>
+                </ElevationScroll>
             </Box>
         </>
 
