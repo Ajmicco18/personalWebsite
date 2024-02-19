5f6126bebbbff83bce71bf39415aab04604fcdd9
Anthony Micco <anthonymicco2003@gmail.com>
2024-02-19 12:11:02 -0500

    fixed unresolved attributes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import Header from './Header.jsx'
 import AboutMe from './AboutMe'
 import Experience from './Experience'
 import Nav from './Nav.jsx'
+import { styled, Container } from '@mui/material'
 
 const Wrapper = styled(Container)(({ theme }) => ({
     maxWidth: "xl",
@@ -24,12 +25,10 @@ const Wrapper = styled(Container)(({ theme }) => ({
 export default function Home() {
     return (
         <>
-            <Wrapper>
-                <Nav></Nav>
-                <Header></Header>
-                <AboutMe></AboutMe>
-                <Experience></Experience>
-            </Wrapper>
+            <Nav></Nav>
+            <Header></Header>
+            <AboutMe></AboutMe>
+            <Experience></Experience>
 
         </>
     )
